17b8485cffe27147ffe0bf5db5afbad0816dc45e
Endel Dreyer <endel.dreyer@gmail.com>
2019-08-19 23:56:32 -0300

    update for v0.11

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import http from "http";
 import express from "express";
+import cors from "cors";
 import { Server } from "colyseus";
 import { monitor } from "@colyseus/monitor";
 import socialRoutes from "@colyseus/social/express"
@@ -9,8 +10,14 @@ import { MyRoom } from "./MyRoom";
 const port = Number(process.env.PORT || 2567);
 const app = express()
 
+app.use(cors());
+app.use(express.json())
+
 const server = http.createServer(app);
-const gameServer = new Server({ server });
+const gameServer = new Server({
+  server,
+  express: app
+});
 
 // register your room handlers
 gameServer.register('my_room', MyRoom);
