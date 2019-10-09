a5d757edc635a4934bf1c5dabeda972428bf3511
Endel Dreyer <endel.dreyer@gmail.com>
2019-10-08 21:44:05 -0300

    format MyRoom

--- MyRoom.ts
+++ MyRoom.ts
@@ -1,9 +1,20 @@
 import { Room, Client } from "colyseus";
 
 export class MyRoom extends Room {
-  onCreate (options: any) {}
-  onJoin (client: Client, options: any) {}
-  onMessage (client: Client, message: any) {}
-  onLeave (client: Client, consented: boolean) {}
-  onDispose() {}
+
+  onCreate (options: any) {
+  }
+
+  onJoin (client: Client, options: any) {
+  }
+
+  onMessage (client: Client, message: any) {
+  }
+
+  onLeave (client: Client, consented: boolean) {
+  }
+
+  onDispose() {
+  }
+
 }
