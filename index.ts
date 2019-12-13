62b5729e444cef62d6afb5020bafafd42f48d018
Endel Dreyer <endel.dreyer@gmail.com>
2019-12-13 16:08:13 -0300

    leave @colyseus/social commented out by default. closes #4

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 import { Server } from "colyseus";
 import { monitor } from "@colyseus/monitor";
-import socialRoutes from "@colyseus/social/express"
+// import socialRoutes from "@colyseus/social/express"
 
 import { MyRoom } from "./MyRoom";
 
@@ -21,8 +21,13 @@ const gameServer = new Server({
 // register your room handlers
 gameServer.define('my_room', MyRoom);
 
-// register @colyseus/social routes
-app.use("/", socialRoutes);
+/**
+ * Register @colyseus/social routes
+ *
+ * - uncomment if you want to use default authentication (https://docs.colyseus.io/authentication/)
+ * - also uncomment the import statement
+ */
+// app.use("/", socialRoutes);
 
 // register colyseus monitor AFTER registering your room handlers
 app.use("/colyseus", monitor(gameServer));
