39b553f97c98e69b3c899b2e05bd3f66ad0e6743
Endel Dreyer <endel.dreyer@gmail.com>
2020-05-02 04:07:27 +0300

    upgrade colyseus/loadtest to 0.13

--- loadtest/example.ts
+++ loadtest/example.ts
@@ -6,20 +6,20 @@ export function requestJoinOptions (this: Client, i: number) {
 
 export function onJoin(this: Room) {
     console.log(this.sessionId, "joined.");
-}
 
-export function onMessage(this: Room, message) {
-    console.log(this.sessionId, "received:", message);
+    this.onMessage("*", (type, message) => {
+        console.log(this.sessionId, "received:", type, message);
+    });
 }
 
 export function onLeave(this: Room) {
     console.log(this.sessionId, "left.");
 }
 
-export function onError(this: Room, err) {
+export function onError(this: Room, err: any) {
     console.log(this.sessionId, "!! ERROR !!", err.message);
 }
 
-export function onStateChange(this: Room, state) {
+export function onStateChange(this: Room, state: any) {
     console.log(this.sessionId, "new state:", state);
 }
