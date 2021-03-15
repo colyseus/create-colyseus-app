878bec9bebc4a506eeabd71b7f03cb598b48cfb8
Endel Dreyer <endel.dreyer@gmail.com>
2021-03-15 18:50:04 -0300

    use @colyseus/arena tool

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,15 @@
-import http from "http";
-import express from "express";
-import cors from "cors";
-import { Server } from "colyseus";
-import { monitor } from "@colyseus/monitor";
-// import socialRoutes from "@colyseus/social/express"
-
-import { MyRoom } from "./rooms/MyRoom";
-
-const port = Number(process.env.PORT || 2567);
-const app = express()
-
-app.use(cors());
-app.use(express.json())
-
-const server = http.createServer(app);
-const gameServer = new Server({
-  server,
-});
-
-// register your room handlers
-gameServer.define('my_room', MyRoom);
-
 /**
- * Register @colyseus/social routes
- *
- * - uncomment if you want to use default authentication (https://docs.colyseus.io/server/authentication/)
- * - also uncomment the import statement
+ * IMPORTANT: 
+ * ---------
+ * Do not manually edit this file if you'd like to use Colyseus Arena
+ * 
+ * If you're self-hosting (without Arena), you can manually instantiate a
+ * Colyseus Server as documented here: 👉 https://docs.colyseus.io/server/api/#constructor-options 
  */
-// app.use("/", socialRoutes);
+import { listen } from "@colyseus/arena";
 
-// register colyseus monitor AFTER registering your room handlers
-app.use("/colyseus", monitor());
+// Import arena config
+import arenaConfig from "./config/arena.config";
 
-gameServer.listen(port);
-console.log(`Listening on ws://localhost:${ port }`)
+// Create and listen on 2567 (or PORT environment variable.)
+listen(arenaConfig);
