323d4c88f4ed33e90d9d6d575a6d7977e9dae142
Endel Dreyer <endel.dreyer@gmail.com>
2021-04-16 10:47:04 -0300

    use generic state type for auto-completion

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -1,7 +1,7 @@
 import { Room, Client } from "colyseus";
 import { MyRoomState } from "./schema/MyRoomState";
 
-export class MyRoom extends Room {
+export class MyRoom extends Room<MyRoomState> {
 
   onCreate (options: any) {
     this.setState(new MyRoomState());
@@ -15,12 +15,15 @@ export class MyRoom extends Room {
   }
 
   onJoin (client: Client, options: any) {
+    console.log(client.sessionId, "joined!");
   }
 
   onLeave (client: Client, consented: boolean) {
+    console.log(client.sessionId, "left!");
   }
 
   onDispose() {
+    console.log("room", this.roomId, "disposing...");
   }
 
 }
